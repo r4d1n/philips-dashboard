8805da523068bbbb511d26c19182887847cf9645
Hao Gao <hao.x.gao@gmail.com>
2015-03-07 13:02:06 -0800

    working sample

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    request({
+        url: "https://gateway.api.pcftest.com:9004/v1/oauth2/token?grant_type=client_credentials",
+        method: "POST",
+        json: true,
+        headers: {
+            'Authorization': "Basic RFJDWUpFYnFQSUJiMmJOZURlSlcyR3V3QTNDUTFqaHk6cHJQTHIzYjJHRktVVFVacw==",
+        },
+        //body: JSON.stringify({username: "sam.s.smith", password: "MyFood4Health!"})
+    } , function( validateError, validateResponse, validateBody ) {
+        //var validationDetails = JSON.parse( validateBody );
+        console.log(validateBody);
+        //console.log(JSON.stringify({username: "sam.s.smith", password: "MyFood4Health!"}));
+        //console.log(validateBody);
+    })
+    res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
