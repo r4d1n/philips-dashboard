0df7e617719f579d3009f3086a6715e0a22837d0
Hao Gao <hao.x.gao@gmail.com>
2015-03-07 14:02:17 -0800

    refactor

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,7 @@ var express = require('express');
 var router = express.Router();
 var request = require('request');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
+function token(fn) {
     request({
         url: "https://gateway.api.pcftest.com:9004/v1/oauth2/token?grant_type=client_credentials",
         method: "POST",
@@ -12,8 +11,19 @@ router.get('/', function(req, res, next) {
             'Authorization': "Basic RFJDWUpFYnFQSUJiMmJOZURlSlcyR3V3QTNDUTFqaHk6cHJQTHIzYjJHRktVVFVacw==",
         },
     } , function( validateError, validateResponse, validateBody ) {
+        fn(validateError, validateResponse, validateBody);
+    })
+}
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.render('index', {});
+});
+
+/* GET home page. */
+router.get('/user', function(req, res, next) {
+    token(function( validateError, validateResponse, validateBody ) {
+        /*
         var accessToken = validateBody.access_token
-        console.log(accessToken);
         request({
             url: "https://gateway.api.pcftest.com:9004/v1/oauth2/authorize/login",
             method: "POST",
@@ -28,8 +38,23 @@ router.get('/', function(req, res, next) {
             res.json(user);
             
         })
+        */
+        var accessToken = validateBody.access_token
+        request({
+            url: "https://gateway.api.pcftest.com:9004/v1/oauth2/authorize/login",
+            method: "GET",
+            json: true,
+            headers: {
+                'Authorization': "Bearer " + accessToken,
+            },
+            body: {username: "sam.s.smith", password: "MyFood4Health!"}
+        } , function( validateError, validateResponse, validateBody ) {
+            var user = validateBody
+            console.log(validateBody);
+            res.json(user);
+            
+        })
     })
-    
 });
 
 module.exports = router;
