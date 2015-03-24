35e5f3d07c2e647f0f4abd28a8fef55155780b34
Jared Radin <jared.radin@gmail.com>
2015-03-24 12:06:30 -0700

    fix broken legends, organize sidebar

--- javascript/client.js
+++ javascript/client.js
@@ -1,128 +1,123 @@
 $(document).ready(function() {
 
-var seriesData1 = [ [], [], [], [], []];
-var seriesData2 = [ [], [], []];
-var random = new Rickshaw.Fixtures.RandomData(50);
-for (var i = 0; i < 75; i++) {
-	random.addData(seriesData1);
-}
-for (var i = 0; i < 75; i++) {
-	random.addData(seriesData2);
-}
-var graph1 = new Rickshaw.Graph( {
-	element: document.getElementById("chart1"),
-	renderer: 'multi',
-	width: 750,
-	height: 250,
-	dotSize: 5,
-	series: [
-		{
-			name: 'Temperature',
-			data: seriesData1.shift(),
-			color: 'rgba(255, 0, 0, 0.4)',
-			renderer: 'line'
-		}, {
-			name: 'Respiratory',
-			data: seriesData1.shift(),
-			color: 'rgba(255, 127, 0, 0.4)',
-			renderer: 'line'
-		}, {
-			name: 'Heartbeat',
-			data: seriesData1.shift(),
-			color: 'rgba(127, 0, 0, 0.3)',
-			renderer: 'line'
-		}, {
-			name: 'Blood Pressure',
-			data: seriesData1.shift().map(function(d) { return { x: d.x, y: d.y / 4 } }),
-			color: 'rgba(0, 0, 127, 0.4)',
-			renderer: 'line'
-		}, {
-			name: 'O2',
-			data: seriesData1.shift().map(function(d) { return { x: d.x, y: d.y * 1.5 } }),
-			renderer: 'line',
-			color: 'rgba(204, 0, 51, 1.0)'
-		}
-	]
+  var seriesData1 = [ [], [], [], [], []];
+  var seriesData2 = [ [], [], []];
+  var random = new Rickshaw.Fixtures.RandomData(50);
+  for (var i = 0; i < 75; i++) {
+    random.addData(seriesData1);
+  }
+  for (var i = 0; i < 75; i++) {
+    random.addData(seriesData2);
+  }
+  var vitalsGraph = new Rickshaw.Graph( {
+    element: document.getElementById("chart1"),
+    renderer: 'multi',
+    width: 750,
+    height: 250,
+    dotSize: 5,
+    series: [
+  {
+    name: 'Temperature',
+    data: seriesData1.shift(),
+    color: 'rgba(255, 0, 0, 0.4)',
+    renderer: 'line'
+  }, {
+    name: 'Respiratory',
+    data: seriesData1.shift(),
+    color: 'rgba(255, 127, 0, 0.4)',
+    renderer: 'line'
+  }, {
+    name: 'Heartbeat',
+    data: seriesData1.shift(),
+    color: 'rgba(127, 0, 0, 0.3)',
+    renderer: 'line'
+  }, {
+    name: 'Blood Pressure',
+    data: seriesData1.shift().map(function(d) { return { x: d.x, y: d.y / 4 } }),
+    color: 'rgba(0, 0, 127, 0.4)',
+    renderer: 'line'
+  }, {
+    name: 'O2',
+    data: seriesData1.shift().map(function(d) { return { x: d.x, y: d.y * 1.5 } }),
+    renderer: 'line',
+    color: 'rgba(204, 0, 51, 1.0)'
+  }
+  ]
 } );
 
 
-var graph2 = new Rickshaw.Graph( {
-	element: document.getElementById("chart2"),
-	renderer: 'multi',
-	width: 750,
-	height: 250,
-	dotSize: 5,
-	series: [
-		{
-			name: 'CBC',
-			data: seriesData2.shift().map(function(d) { return { x: d.x, y: d.y * 1.5 } }),
-			renderer: 'line',
-			color: 'rgba(0, 0, 127, 0.25)'
-		}, {
-			name: 'BMP',
-			data: seriesData2.shift().map(function(d) { return { x: d.x, y: d.y * 1.5 } }),
-			renderer: 'line',
-			color: 'rgba(0, 0, 127, 0.25)'
-		}, {
-			name: 'Blood Culture',
-			data: seriesData2.shift().map(function(d) { return { x: d.x, y: d.y * 1.5 } }),
-			renderer: 'line',
-			color: 'rgba(0, 0, 127, 0.25)'
-		}
-	]
+var labsGraph = new Rickshaw.Graph( {
+  element: document.getElementById("chart2"),
+  renderer: 'multi',
+  width: 750,
+  height: 250,
+  dotSize: 5,
+  series: [
+{
+  name: 'CBC',
+  data: seriesData2.shift().map(function(d) { return { x: d.x, y: d.y * 1.5 } }),
+  renderer: 'line',
+  color: 'rgba(0, 0, 127, 0.25)'
+}, {
+  name: 'BMP',
+  data: seriesData2.shift().map(function(d) { return { x: d.x, y: d.y * 1.5 } }),
+  renderer: 'line',
+  color: 'rgba(0, 0, 127, 0.25)'
+}, {
+  name: 'Blood Culture',
+  data: seriesData2.shift().map(function(d) { return { x: d.x, y: d.y * 1.5 } }),
+  renderer: 'line',
+  color: 'rgba(0, 0, 127, 0.25)'
+}
+]
 } );
 
 
 var slider = new Rickshaw.Graph.RangeSlider.Preview({
-	graphs: [graph1, graph2],
-	element: document.querySelector('#slider')
+  graphs: [vitalsGraph, labsGraph],
+  element: document.querySelector('#slider')
 });
 // var slider = new Rickshaw.Graph.RangeSlider.Preview({
-// 	graph: graph2,
+// 	graph: labsGraph,
 // 	element: document.querySelector('#slider')
 // });
-graph1.render();
-graph2.render();
+vitalsGraph.render();
+labsGraph.render();
 
-// graph 1 stuffs
+// vitalsGraph stuffs
 var detail1 = new Rickshaw.Graph.HoverDetail({
-	graph: graph1
+  graph: vitalsGraph
 });
-var legend = new Rickshaw.Graph.Legend({
-	graph: graph1,
-	element: document.querySelector('#legend')
+var vitalsLegend = new Rickshaw.Graph.Legend({
+  graph: vitalsGraph,
+  element: document.querySelector('#vitals-legend')
 });
 var highlighter = new Rickshaw.Graph.Behavior.Series.Highlight({
-    graph: graph1,
-    legend: legend,
-    disabledColor: function() { return 'rgba(0, 0, 0, 0.2)' }
+  graph: vitalsGraph,
+  legend: vitalsLegend,
+  disabledColor: function() { return 'rgba(0, 0, 0, 0.2)' }
 });
 
 var highlighter = new Rickshaw.Graph.Behavior.Series.Toggle({
-    graph: graph2,
-    legend: legend
+  graph: vitalsGraph,
+  legend: vitalsLegend
 });
 
-//graph 2 stuff
+// labsGraph stuff
 var detail2 = new Rickshaw.Graph.HoverDetail({
-	graph: graph2
+  graph: labsGraph
 });
-var legend = new Rickshaw.Graph.Legend({
-	graph: graph2,
-	element: document.querySelector('#legend')
+var labsLegend = new Rickshaw.Graph.Legend({
+  graph: labsGraph,
+  element: document.querySelector('#labs-legend')
 });
 var highlighter = new Rickshaw.Graph.Behavior.Series.Highlight({
-    graph: graph2,
-    legend: legend,
-    disabledColor: function() { return 'rgba(0, 0, 0, 0.2)' }
+  graph: labsGraph,
+  legend: labsLegend,
+  disabledColor: function() { return 'rgba(0, 0, 0, 0.2)' }
 });
 var highlighter = new Rickshaw.Graph.Behavior.Series.Toggle({
-    graph: graph2,
-    legend: legend
+  graph: labsGraph,
+  legend: labsLegend
 });
-
-
-
-
-
 });
